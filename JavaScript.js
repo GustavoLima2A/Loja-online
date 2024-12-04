f83d8f920641fdf88be7ccaf728a35411a061f46
Gustagol-07 <00001206255055sp@al.educacao.sp.gov.br>
2024-12-04 17:57:14 -0300

    gsu

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    const searchQuery = document.getElementById('search').value;
+    if (searchQuery) {
+      window.location.href = `/search?q=${searchQuery}`;
+    }
+  });
+  
